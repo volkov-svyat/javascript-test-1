af6e494621662c4fad1ec615ce983dddb1e14945
Cybersport <Cybersport@26kadr.internal>
2024-05-28 14:00:52 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -183,3 +183,18 @@ function task_23() {
         alert("Я вас не знаю");
     };
 };
+
+//
+
+function add(a,b) {
+    return a+b;
+};
+function sub(a,b) {
+    return a-b;
+};
+function div(a,b) {
+    return a/b;
+};
+function mul(a,b) {
+    return a*b;
+};
